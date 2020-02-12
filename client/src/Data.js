72339011bc12d8f7b96866591fef145a2b9c17e4
enaughton <enaughton5@gmail.com>
2020-02-11 21:03:28 -0500

    revert

--- client/src/Data.js
+++ client/src/Data.js
@@ -46,6 +46,7 @@ export default class Data {
 
   async createUser(user) {
     const response = await this.api("/users", "POST", user);
+    console.log(response);
     if (response.status === 201) {
       return [];
     } else if (response.status === 400) {
